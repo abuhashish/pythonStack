63368e10c7fcc6f36e96a8926562b769951be9c8
top1million <aasahashish@gmail.com>
2021-05-23 14:24:14 +0300

    hello

--- django/django_orm/DojoShell/shellapp/urls.py
+++ django/django_orm/DojoShell/shellapp/urls.py
@@ -2,4 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('addDojo',views.addDojo),
+    path('addNinja',views.addNinja),
+    path('delete/<int:x>',views.delete)
 ]
--- django/django_orm/DojoShell/shellapp/views.py
+++ django/django_orm/DojoShell/shellapp/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import redirect, render,HttpResponse
+from shellapp.models import *
 # Create your views here.
 def index(request):
-    return HttpResponse("hello")
+    context={
+        'all':Dojo.objects.all()
+    }
+    return render(request,"index.html",context)
+def addDojo(request):
+    Dojo.objects.create(name=request.POST['name'],city=request.POST['city'],state=request.POST['state'])
+    return redirect('/')
+def addNinja(request):
+    print(request.POST['dojo'])
+    x=Dojo.objects.get(id=request.POST['dojo'])
+    Ninja.objects.create(first_name=request.POST['fname'],last_name=request.POST['lname'],dojo=x)
+    return redirect('/')
+def delete(request,x):
+    delx=Dojo.objects.get(id=x)
+    delx.delete()
+    return redirect('/')
