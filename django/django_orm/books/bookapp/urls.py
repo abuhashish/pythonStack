bc8163301df9369d01e6b415679addc0a4ab0993
top1million <aasahashish@gmail.com>
2021-05-23 16:57:00 +0300

    hello

--- django/django_orm/books/bookapp/urls.py
+++ django/django_orm/books/bookapp/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('addbook',views.addbook),
-    path('books/<int:id>',views.viewbook)
+    path('books/<int:id>',views.viewbook),
+    path('authors',views.authors),
+    path('addauthor',views.addauthor),
+    path('authors/<int:id>',views.author),
+    path('addbookauthor/<int:id>',views.addbookauthor),
+    path('addauthorbook/<int:id>',views.addauthorbook)
 ]
--- django/django_orm/books/bookapp/views.py
+++ django/django_orm/books/bookapp/views.py
@@ -11,6 +11,35 @@ def addbook(request):
     return redirect('/')
 def viewbook(request,id):
     context={
-        'book':Book.objects.get(id=id)
+        'book':Book.objects.get(id=id),
+        'z':Author.objects.all()
     }
-    return redirect(request,"view.html",context)
+    
+    
+    return render(request,"view.html",context)
+def authors(request):
+    context={
+        'x':Author.objects.all()
+    }
+    return render(request,"authors.html",context)
+def author(request,id):
+    x=Author.objects.get(id=id)
+    z=Book.objects.all()
+    context={
+        'x':x,
+        'z':z
+    }
+    return render(request,"author.html",context)
+def addauthor(request):
+    Author.objects.create(first_name=request.POST['fname'],last_name=request.POST['lname'],notes=request.POST['notes'])
+    return redirect('/')
+def addbookauthor(request,id):
+    x=Author.objects.get(id=id)
+    i=request.POST['id']
+    x.books.add(Book.objects.get(id=i))
+    return redirect('/authors/'+str(id))
+def addauthorbook(request,id):
+    x=Book.objects.get(id=id)
+    i=request.POST['id']
+    x.authors.add(Author.objects.get(id=i))
+    return redirect('/books/'+str(id))
