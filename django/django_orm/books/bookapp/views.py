db2303a14cf07f1aa07031afef5f878df6d94c08
top1million <aasahashish@gmail.com>
2021-05-23 15:45:38 +0300

    hello

--- django/django_orm/books/bookapp/views.py
+++ django/django_orm/books/bookapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import redirect, render,HttpResponse
+from bookapp.models import *
 # Create your views here.
-def index(reqeust):
-    return HttpResponse("hello")
+def index(request):
+    context={
+        'x':Book.objects.all()
+            }
+    return render(request,"index.html",context)
+def addbook(request):
+    Book.objects.create(title=request.POST['title'],desc=request.POST['desc'])
+    return redirect('/')
